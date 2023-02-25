58012529fbd009ac47cbf90487d0f8b1266a0cf8
Natan Tourne <natan.tourne@gmail.com>
2023-02-25 08:02:06 +0100

    ??

--- plmbind/data_processing/Get_protein_seqs.py
+++ plmbind/data_processing/Get_protein_seqs.py
@@ -1,4 +1,3 @@
-import argparse
 import numpy as np
 import h5torch
 from Bio import SeqIO
@@ -7,12 +6,7 @@ from transformers import pipeline
 from transformers import AutoTokenizer
 from transformers import EsmModel, EsmConfig, EsmTokenizer
 import requests
-
-parser = argparse.ArgumentParser()
-parser.add_argument("path")
-parser.add_argument("embeddings")
-parser.add_argument("embeddings_name")
-args = parser.parse_args()
+from tqdm import tqdm, trange
 
 def get_protein_sequence(TF):
     r = requests.get(f'http://remap.univ-amu.fr/api/v1/info/byTarget/target={TF}&taxid=9606')
@@ -27,24 +21,16 @@ def get_protein_sequence(TF):
             fasta_sequences = SeqIO.parse(StringIO(r1.text),'fasta')
             protein_sequence = list(fasta_sequences)[0].seq
             return protein_sequence
-        
-f = h5torch.File(args.path, "a")
-
-embeddings = args.embeddings
-# embeddings = "facebook/esm2_t33_650M_UR50D"
-# Protein features
-tokenizer = EsmTokenizer.from_pretrained(embeddings)
-model = EsmModel.from_pretrained(embeddings)
+h5_path = "/home/natant/Thesis/Data/ReMap_testing/train.h5t"
+out_path = "/home/natant/Thesis/plmbind/data_processing/TF_sequences.fasta"
+f = h5torch.File(h5_path, "a")
 TF_names = f["unstructured/protein_map"][:].astype("str")
-embedding_list = []
-for TF in TF_names:
-    print(TF)
-    protein_seq = str(get_protein_sequence(TF))
-    inputs = tokenizer(protein_seq, return_tensors="pt")
-    outputs = model(**inputs)
-    last_hidden_states = outputs.last_hidden_state[0].detach().mean(axis=0).numpy()
-    embedding_list.append(last_hidden_states)
-    
-f.register(np.array(embedding_list), axis = "unstructured", mode="separate", name=args.embeddings_name, dtype_save = "float32", dtype_load = "float32")
 f.close()
-print("done")
+protein_seqs = []
+for TF in tqdm(TF_names, desc="Fetching TF sequences"):
+    protein_seqs.append(str(get_protein_sequence(TF)))
+    
+with open(out_path, 'w') as f:
+    for i, seq in enumerate(protein_seqs):
+        f.write(">" + TF_names[i] + "\n" + seq + "\n")
+    
